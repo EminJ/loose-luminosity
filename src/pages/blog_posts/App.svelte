4a07bde130c1fc51c0795102fc84b79180fe98a4
Muhammet Emin Güleşci <114227722+EminJ@users.noreply.github.com>
2022-12-09 15:47:26 +0300

    dara

--- src/pages/blog_posts/App.svelte
+++ src/pages/blog_posts/App.svelte
@@ -4,10 +4,14 @@
 	import List from './components/List.svelte';
   
 	let postsInfo=[];
+	let postsInfo2=[];
   onMount(async () => {
     const { data } = await axios.get('http://3.213.186.72:5678/api/show/post');
     postsInfo = data.blogposts;
+    const { data2 } = await axios.get('https://sponsors.vuejs.org/data.json');
+    postsInfo2 = data2;
   });
   
 </script>
+
 <List postsArray={postsInfo}/>
