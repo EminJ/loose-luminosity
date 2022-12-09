b968cb283035798b7bed3506b134c41a7adfc166
Muhammet Emin Güleşci <114227722+EminJ@users.noreply.github.com>
2022-12-09 17:34:51 +0300

    🎈

--- src/pages/blog_posts/App.svelte
+++ src/pages/blog_posts/App.svelte
@@ -5,7 +5,7 @@
   
 	let postsInfo=[];
   onMount(async () => {
-    const { data } = await axios.get('http://54.157.69.5:5678/api/show/post');
+    const { data } = await axios.get('http://3.87.32.105:5678/api/show/post');
     postsInfo = data.blogposts;
   });
   
