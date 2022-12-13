21ffc6c9c7abd289c09b911f7b373802599b8dfb
Muhammet Emin Güleşci <114227722+EminJ@users.noreply.github.com>
2022-12-13 19:53:43 +0300

    https

--- src/pages/blog_posts/App.svelte
+++ src/pages/blog_posts/App.svelte
@@ -5,7 +5,7 @@
   
 	let postsInfo=[];
   onMount(async () => {
-    const options = {method: 'GET', url: 'http://191.101.1.221:5678/api/show/post'};
+    const options = {method: 'GET', url: 'https://191.101.1.221:5678/api/show/post'};
   axios.request(options).then(function (response) {
     postsInfo = response.data.blogposts;
   }).catch(function (error) {
